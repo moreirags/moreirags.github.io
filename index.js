44a2859261dcbf082ca9cd4811acf0251cb162f4
Giovana Moreira <giovanamoreirasilva@outlook.com>
2024-10-21 13:12:10 -0300

    Commit 2

---
+++ index.js
@@ -0,0 +1,37 @@
+﻿function toggleContent(id) {
+    var idText = "text" + id;
+    var idArrow = "arrow" + id;
+
+    var content = document.getElementById(idText);
+    var arrow = document.getElementById(idArrow);
+
+    if (content.style.display == "none") {
+        if (idText == "text3") 
+            content.style.display = "flex";        
+        else
+            content.style.display = "block";
+
+        arrow.classList.add("rotate");
+    }
+    else {
+        content.style.display = "none";
+        arrow.classList.remove("rotate");
+    }
+
+    adjustFooter();
+}
+
+function adjustFooter() {
+    const footer = document.querySelector('footer');
+    const windowHeight = window.innerHeight;
+    const contentHeight = document.body.scrollHeight;
+
+    if (contentHeight < windowHeight) {
+        footer.style.position = 'absolute';
+        footer.style.bottom = '0';
+    } else {
+        footer.style.position = 'relative';
+    }
+}
+
+window.onload = adjustFooter;
